bef39eaed602ddd42c168bb879b6e9754464b60b
aalimran <tfthemecafe@gmail.com>
2018-11-11 17:08:29 +0600

    I have delete one line from the frontpage.php

--- frontpage.php
+++ frontpage.php
@@ -6,6 +6,5 @@
 get_header();
 get_template_part( 'template-parts/section/home', 'slider' );
 get_template_part( 'template-parts/section/blog', 'grid' );
-get_template_part( 'template-parts/section/post', 'filter' );
 get_footer();
 ?>
